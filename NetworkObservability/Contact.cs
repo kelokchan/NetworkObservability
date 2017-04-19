25ef0b2409a78e4c66892c06a41066cc9e57305f
Kelok <kelokchan@gmail.com>
2017-04-19 16:51:15 +1000

    added drag n drop testing code

---
+++ NetworkObservability/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkObservability
+{
+    public class Contact
+    {
+        public Contact(string firstname, string lastname)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Firstname: {0}, Lastname: {1}",Firstname, Lastname);
+        }
+    }
+}
--- NetworkObservability/MainWindow.xaml.cs
+++ NetworkObservability/MainWindow.xaml.cs
@@ -24,5 +24,38 @@ namespace NetworkObservability
         {
             InitializeComponent();
         }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Label lblFrom = e.Source as Label;
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragDrop.DoDragDrop(lblFrom, lblFrom.Content, DragDropEffects.Copy);
+        }
+
+        private void Label_QueryContinueDrag(object sender, QueryContinueDragEventArgs e)
+        {
+            Label lblFrom = e.Source as Label;
+
+            if (!e.KeyStates.HasFlag(DragDropKeyStates.LeftMouseButton))
+                lblFrom.Content = "...";
+        }
+
+        private void Label_Drop(object sender, DragEventArgs e)
+        {
+
+            string draggedText = (string)e.Data.GetData(DataFormats.StringFormat);
+
+            Label toLabel = e.Source as Label;
+            toLabel.Content = draggedText;
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            System.Windows.Point p2 = e.GetPosition(this);
+            lblInfo2.Content = string.Format("DragEventArgs.GetPosition: {0}, {1}", p2.X, p2.Y);
+        }
     }
+
+   
 }
