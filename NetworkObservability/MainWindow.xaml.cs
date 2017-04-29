c0180aa390d7c62346cc155e535313562325cb31
Kelok <kelokchan@gmail.com>
2017-04-29 17:27:58 +1000

    Added drag and drop test

--- NetworkObservability/MainWindow.xaml.cs
+++ NetworkObservability/MainWindow.xaml.cs
@@ -23,8 +23,40 @@ namespace NetworkObservability
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = new List<DateTime>
+            {
+                DateTime.Now,
+                new DateTime(2013, 02, 13),
+                new DateTime(2004, 12, 31)
+            };
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DataObject data = new DataObject(DataFormats.Serializable, "Hi");
+
+            DragDrop.DoDragDrop((DependencyObject)e.Source, data, DragDropEffects.Copy);
+
+        }
+
+        private void Label_QueryContinueDrag(object sender, QueryContinueDragEventArgs e)
+        {
+
+        }
+
+        private void Canvas_Drop(object sender, DragEventArgs e)
+        {
+            String test = (String)e.Data.GetData(DataFormats.Serializable);
+            Point p = Mouse.GetPosition(this);
+
+            Canvas.SetLeft(canvas, p.X);
+            Canvas.SetTop(canvas, p.Y);
+
+
+           // canvas.Children.Add(test);
         }
     }
 
-   
+
 }
