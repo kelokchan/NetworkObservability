17590c5f32499ec9cf8753008620931aa1ca0f1f
Kelok <kelokchan@gmail.com>
2017-05-09 17:12:22 +1000

    code testing for custom front end Node class

---
+++ NetworkObservability/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace NetworkObservability
+{
+    class Node : Image
+    {
+        public Node(string name, int id)
+        {
+            this.Name = name;
+            this.ID = id;
+        }
+
+        public int ID { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
