c0a6cbfb0085ce87a34546b64113d32cd9483cf7
Chun Huei <fchunhuei@gmail.com>
2017-08-27 18:37:08 +1000

    Created CanvasGraphXML

---
+++ NetworkObservability/CanvasGraphXML.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkObservabilityCore;
+
+namespace NetworkObservability
+{
+	public class CanvasGraphXML : GraphXML
+	{
+		#region Constructors
+		public CanvasGraphXML() 
+			: base()
+		{
+		}
+
+		public CanvasGraphXML(String version, String encoding, String standalone) 
+			: base(version, encoding, standalone)
+		{
+		}
+		#endregion
+
+
+
+	}
+}
