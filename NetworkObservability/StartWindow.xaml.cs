2000455074433f4b7cc511a3d895ac29284dfe62
MxtfV <mustafa.logan@outlook.com.au>
2017-09-10 20:41:13 +1000

    Edited "StartWindow" class

--- NetworkObservability/StartWindow.xaml.cs
+++ NetworkObservability/StartWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using NetworkObservabilityCore;
 
 namespace NetworkObservability
 {
@@ -19,6 +20,11 @@ namespace NetworkObservability
     /// </summary>
     public partial class StartWindow : Window
     {
+        // A tuple of 2 elements.
+        // first is a name of attribute, which is a key to be used by algorithms,
+        // the second one is a constraint object.
+        Tuple<string, Constraint<IEdge>> returnValue;
+
         public StartWindow()
         {
             InitializeComponent();
